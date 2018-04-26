22ad7cf8bfcc9af46a20dc5b39f05b3bd7c73cc0
Bryan McAfee <brymcafee@gmail.com>
2018-04-26 14:43:03 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,6 @@
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
 def using_include(array, element)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array.include(element)
 end
 
